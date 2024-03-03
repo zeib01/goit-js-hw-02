c4c472fb12daa0a4e03dbf720cedeb2e39582da4
zeib01 <ibragimovazeinep@gmail.com>
2024-03-03 14:05:30 +0100

    task 1, 2, 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,20 @@
-function getElementWidth(content, padding, border) {
-
-    return Number.parseFloat(content) + (Number.parseFloat(padding) + Number.parseFloat(border)) * 2;
+function checkForSpam(message) {
+    const verifiedMessage = message.toLowerCase();
+    console.log(verifiedMessage);
+    if (verifiedMessage.includes("spam")) {
+        return true;
+    }
+    else if (verifiedMessage.includes("sale")) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
